968748cc4fd914cdda1befe981a1a1ce248cb3b0
SeungWon <20sunrin041@sunrint.hs.kr>
2021-07-13 12:14:27 +0900

    feat: Add permissions

---
+++ src/lib/permissions.ts
@@ -0,0 +1,18 @@
+import { rule, shield } from "graphql-shield"
+import { ApolloError } from "apollo-server-express"
+
+const isValid = rule()((parent: void, args: void, { uName }: { uName: string | null }) => {
+    if (uName === null) {
+        return new ApolloError("check authorization token")
+    }
+    return true
+})
+
+export const permissions = shield({
+    Mutation: {
+
+    },
+    Query: {
+
+    }
+})
